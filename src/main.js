6ec19a2917f7265c010a09703356793d9d595ba3
Gusted <williamzijl7@hotmail.com>
2020-05-25 22:39:50 +0200

    Week 1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,85 @@
-function start() {
-    DarkReader.enable();
+(function () {
+    'use strict'
+
+let audio = new Audio();
+audio.src = 'cynamatics.mp3';
+audio.loop = true;
+audio.autoplay = true;
+audio.crossOrigin = "anonymous";
+
+// Define variables for analyser
+let audioContext, analyser, source, fbc_array, data, len, total;
+
+// Define Audio Analyser Helpers
+function createAudioContext() {
+    audioContext = new (window.AudioContext || window.webkitAudioContext);
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 1024;
+    len = analyser.fftSize / 16;
+    source = audioContext.createMediaElementSource(audio);
+    source.connect(analyser);
+    analyser.connect(audioContext.destination);
 }
 
-start();
+
+let canvas, canvasCtx;
+
+function createCanvas() {
+    canvas = document.getElementById('analyser');
+    canvasCtx = canvas.getContext('2d');
+}
+
+function defineSizesCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+let i,
+    cx, cy,
+    beginAngle = 0,
+    angle,
+    twoPI = 2 * Math.PI,
+    angleGap = twoPI / 3,
+    color = 'rgba(115, 226, 36, 0.5)';
+
+// Create the animation
+function frameLooper() {
+    requestAnimationFrame(frameLooper);
+    fbc_array = new Uint8Array(analyser.frequencyBinCount);
+
+    canvasCtx.save();
+    analyser.getByteFrequencyData(fbc_array);
+    data = fbc_array;
+    angle = beginAngle;
+    cx = canvas.width / 2;
+    cy = canvas.height / 2;
+    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+    canvasCtx.strokeStyle = color;
+    canvasCtx.globalCompositeOperation = 'lighter';
+    canvasCtx.lineWidth = 10;
+    total = 0;
+    for (i = 8; i < len; i += 2) {
+        angle += 0.2;
+        canvasCtx.beginPath();
+        canvasCtx.moveTo(cx + data[i] * Math.sin(angle), cy + data[i] * Math.cos(angle));
+        canvasCtx.lineTo(cx + data[i] * Math.sin(angle + angleGap), cy + data[i] * Math.cos(angle + angleGap));
+        canvasCtx.lineTo(cx + data[i] * Math.sin(angle + angleGap * 2), cy + data[i] * Math.cos(angle + angleGap * 2));
+        canvasCtx.closePath();
+        canvasCtx.stroke();
+        total += data[i];
+    }
+    beginAngle = (beginAngle + 0.00001 * total) % twoPI;
+    canvasCtx.restore();
+}
+
+// call the magic =D
+function init() {
+    createAudioContext();
+    createCanvas();
+    defineSizesCanvas();
+    frameLooper();
+}
+
+window.addEventListener('load', init, false);
+window.addEventListener('resize', defineSizesCanvas, false);
+})
